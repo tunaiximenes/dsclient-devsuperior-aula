7a9b2de2cb71fbfc9802f3040227f5971aa7b1c4
Tunai Ximenes <tunai.ximenes@gmail.com>
2022-03-02 22:50:36 -0300

    seeding sql data

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -1,4 +1,22 @@
 package com.devsuperior.dsclient.entities;
 
-public class Client {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String cpf;
+    private Instant birthdate;
+    private Double income;
+    private Integer children;
+
 }
